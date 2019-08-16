c295b24786b0f678e6ff6828ecf08bec23c1b25c
Mustapha Kadri <mustapha.kadri66@gmail.com>
2019-08-16 19:44:20 +0200

    feat(property) : create property entity

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,10 +11,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class PropertyController extends AbstractController {
 
     /** 
+    * @var PropertyRepository
+    */
+    private $repository;
+
+    public function __construct(PropertyRepository $repository){
+        $this->repository = $repository;
+    }
+
+    /** 
     * @Route("/biens", name="property_index")
     * @return Response
     */
     public function index(): Response {
+        $property = $this->repository->findAll();
+        dump($property);
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties'
         ]);
